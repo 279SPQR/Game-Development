e510ee276ade4a0d84911c6321fe7a2fb043fa05
Unknown <zubzub123@gmail.com>
2017-10-13 13:12:06 -0600

    Created Ghosts

---
+++ Ghost House1/Assets/Scripts/GhostAI.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GhostAI : MonoBehaviour {
+
+	public float moveSpeed;
+	public Transform target;
+	public int damage;
+
+	void OnTriggerStay(Collider other)
+	{
+		if(other.gameObject.name == "Player")
+		{
+			// Ghost looks at player
+			transform.LookAt(target);
+			// Ghost moves towards player
+			transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+		}
+	}
+
+
+	// Use this for initialization
+	//void Start () {
+	
+	//}
+	
+	// Update is called once per frame
+	//void Update () {
+	
+	//}
+}
